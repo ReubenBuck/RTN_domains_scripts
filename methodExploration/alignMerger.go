f95099090c1cbe030a5f76cb354c803548384210
ReubenBuckley <reuben.buckley@adelaide.edu.au>
2016-11-12 19:08:08 +1030

    alignment script now working, need to write out the results

--- methodExploration/alignMerger.go
+++ methodExploration/alignMerger.go
@@ -55,6 +55,24 @@ type alignChain struct {
 
 type alignChains []alignChain
 
+type rangeObject struct {
+	refName   string
+	refSize   int
+	refStrand string
+	refStart  int
+	refEnd    int
+	queName   string
+	queSize   int
+	queStrand string
+	queStart  int
+	queEnd    int
+	id        int
+	refGap    int
+	queGap    int
+}
+
+type rangeObjects []rangeObject
+
 var aligns alignRanges
 var chains alignChains
 
@@ -115,19 +133,63 @@ func main() {
 	// 	fmt.Println(chains[i].alignments)
 	// }
 
-	minAlign := 6
+	var rangeGenome rangeObject
+	var rangeGenomes rangeObjects
+	minAlign := 10
 
 	for i := 0; i < len(chains); i++ {
 		var newAlign alignRangeMerged
+		var refGap int
+		var queGap int
 		fmt.Println(chains[i].header)
+		firstTime := true
 		for j := 0; j < len(chains[i].alignments); j++ {
 
 			newAlign.refAlignLen = newAlign.refAlignLen + chains[i].alignments[j].refAlignLen
 			newAlign.queAlignLen = newAlign.queAlignLen + chains[i].alignments[j].queAlignLen
+
 			if chains[i].alignments[j].queGapLen > minAlign || chains[i].alignments[j].refGapLen > minAlign {
 				newAlign.refGapLen = chains[i].alignments[j].refGapLen
 				newAlign.queGapLen = chains[i].alignments[j].queGapLen
+
+				// there will probably need to be an if conditional here
+				rangeGenome.refName = chains[i].header.refName
+				rangeGenome.queName = chains[i].header.queName
+				rangeGenome.refStrand = chains[i].header.refStrand
+				rangeGenome.queStrand = chains[i].header.queStrand
+				rangeGenome.refSize = chains[i].header.refSize
+				rangeGenome.queSize = chains[i].header.queSize
+				rangeGenome.id = chains[i].header.id
+				rangeGenome.refGap = newAlign.refMergedGapLen
+				rangeGenome.queGap = newAlign.queMergedGapLen
+
 				fmt.Println(newAlign)
+
+				if firstTime {
+					rangeGenome.refStart = chains[i].header.refStart
+					rangeGenome.refEnd = rangeGenome.refStart + newAlign.refAlignLen
+
+					rangeGenome.queStart = chains[i].header.queStart
+					rangeGenome.queEnd = rangeGenome.queStart + newAlign.queAlignLen
+
+					rangeGenomes = append(rangeGenomes, rangeGenome)
+					refGap = newAlign.refGapLen
+					queGap = newAlign.queGapLen
+
+					firstTime = false
+
+				} else {
+					rangeGenome.refStart = refGap + rangeGenomes[len(rangeGenomes)-1].refEnd
+					rangeGenome.refEnd = newAlign.refAlignLen + rangeGenome.refStart
+
+					rangeGenome.queStart = queGap + rangeGenomes[len(rangeGenomes)-1].queEnd
+					rangeGenome.queEnd = newAlign.queAlignLen + rangeGenome.queStart
+
+					rangeGenomes = append(rangeGenomes, rangeGenome)
+
+					refGap = newAlign.refGapLen
+					queGap = newAlign.queGapLen
+				}
 
 				newAlign.refAlignLen = 0
 				newAlign.queAlignLen = 0
@@ -148,9 +210,53 @@ func main() {
 			// just get this summing working
 
 		}
+
+		rangeGenome.refName = chains[i].header.refName
+		rangeGenome.queName = chains[i].header.queName
+		rangeGenome.refStrand = chains[i].header.refStrand
+		rangeGenome.queStrand = chains[i].header.queStrand
+		rangeGenome.refSize = chains[i].header.refSize
+		rangeGenome.queSize = chains[i].header.queSize
+		rangeGenome.id = chains[i].header.id
+		rangeGenome.refGap = newAlign.refMergedGapLen
+		rangeGenome.queGap = newAlign.queMergedGapLen
+
+		if firstTime {
+			rangeGenome.refStart = chains[i].header.refStart
+			rangeGenome.refEnd = rangeGenome.refStart + newAlign.refAlignLen
+
+			rangeGenome.queStart = chains[i].header.queStart
+			rangeGenome.queEnd = rangeGenome.queStart + newAlign.queAlignLen
+
+			rangeGenomes = append(rangeGenomes, rangeGenome)
+			refGap = newAlign.refGapLen
+			queGap = newAlign.queGapLen
+
+			firstTime = false
+
+		} else {
+			rangeGenome.refStart = refGap + rangeGenomes[len(rangeGenomes)-1].refEnd
+			rangeGenome.refEnd = newAlign.refAlignLen + rangeGenome.refStart
+
+			rangeGenome.queStart = queGap + rangeGenomes[len(rangeGenomes)-1].queEnd
+			rangeGenome.queEnd = newAlign.queAlignLen + rangeGenome.queStart
+
+			rangeGenomes = append(rangeGenomes, rangeGenome)
+
+			refGap = newAlign.refGapLen
+			queGap = newAlign.queGapLen
+		}
+
 		fmt.Println(newAlign)
 		// here we also need to push out ranged data
 	}
+
+	for i := 0; i < len(rangeGenomes); i++ {
+		fmt.Println(rangeGenomes[i])
+
+	}
+
+	// will read out as two seperate ranges on the same line
 
 	// data is read in and intervals are merging
 	// do we want to read them out as their merged version or would it be better to do it as two seperate ranges
