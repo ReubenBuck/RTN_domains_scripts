002334a601272e355a271efb699a6ac2c54bb2d0
ReubenBuckley <reuben.buckley@adelaide.edu.au>
2017-03-01 11:19:35 +1030

    deduplication

--- comparativeGenomics/candidateInDel.R
+++ comparativeGenomics/candidateInDel.R
@@ -70,21 +70,10 @@ for(ref in genomeRefNames){
 allDel.gr <- sort.GenomicRanges(allDel.gr)
 allIns.gr <- sort.GenomicRanges(allIns.gr)
 
-# we can read them all in and get the interesting pairs
-
-
-
-# should make it like 10% the size of the smaller element. 
-
 
 olDel <- findOverlaps(allDel.gr)
 
 supDel <- olDel[mcols(allDel.gr)$genome[queryHits(olDel)] != mcols(allDel.gr)$genome[subjectHits(olDel)]]
-# layout(1)
-# smoothScatter(log10(mcols(allDel.gr)$gapWidth[queryHits(supDel)]), log10(mcols(allDel.gr)$gapWidth[subjectHits(supDel)]), 
-#               xlim = c(1,3.5), nrpoints = 0, ylim = c(1,3.5), xaxs = "i", yaxs = "i")
-# lines(log10(seq(1,100000,by = 1000)),log10(seq(1,100000,by = 1000)+ (.2* seq(1,100000,by = 1000))), col = 2)
-# lines(log10(seq(1,100000,by = 1000)),log10(seq(1,100000,by = 1000)- (.2* seq(1,100000,by = 1000))), col = 2)
 
 olWidths <- data.frame(queryHitWidth = mcols(allDel.gr)$gapWidth[queryHits(supDel)],
                        subjectHitWidth = mcols(allDel.gr)$gapWidth[subjectHits(supDel)])
@@ -93,7 +82,9 @@ canDel.gr <- allDel.gr[queryHits(supDel)[
   abs(olWidths$queryHitWidth - olWidths$subjectHitWidth) < .2 * apply(X = olWidths,FUN = min,MARGIN = 1)]
   ]
 
-canDel.gr <- makeGRangesFromDataFrame(unique(as.data.frame(canDel.gr)))
+# so we want only unique ranges, 
+canDel.gr <- canDel.gr[!(duplicated(mcols(canDel.gr)))]
+
 mcols(canDel.gr)$subjectID <- paste("del:", 1:length(canDel.gr), sep = "")
 ReduceCanDel.gr <- reduce(resize(canDel.gr, width = 1, fix = "center"))
 
@@ -119,39 +110,20 @@ df <- data.frame(supGenNo, supGenName, supGapWidth, supGapMean, subjectIDs, inde
 mcols(ReduceCanDel.gr) <- df 
 
 
-# ReduceCanDel.gr[supGenNo == 4]
-# 
-# hist(((mcols(refDel.gr)$gapWidth)), breaks = 100)
-# hist(((mcols(ReduceCanDel.gr[mcols(ReduceCanDel.gr)$supGenNo >1])$supGapMean)), breaks = 100, add = TRUE, col = 2, density = 0)
-# 
-# plot(density(log10((mcols(ReduceCanDel.gr[mcols(ReduceCanDel.gr)$supGenNo == 2])$supGapMean))))
-# for(i in 3:9){
-# lines(density(log10((mcols(ReduceCanDel.gr[mcols(ReduceCanDel.gr)$supGenNo == i])$supGapMean))))
-# }
-# ReduceCanDel.gr[overlapsAny(ReduceCanDel.gr,ReduceCanDel.gr[mcols(ReduceCanDel.gr)$subjectHits == "170770,170771,170772"], maxgap = 200)]
-
-
-
 
 olIns <- findOverlaps(allIns.gr)
 
 supIns <- olIns[mcols(allIns.gr)$genome[queryHits(olIns)] != mcols(allIns.gr)$genome[subjectHits(olIns)]]
-# layout(1)
-# # pair selection look like this
-# smoothScatter(log10(mcols(allIns.gr)$gapWidth[queryHits(supIns)]), log10(mcols(allIns.gr)$gapWidth[subjectHits(supIns)]), 
-#               xlim = c(1,5), nrpoints = 0, ylim = c(1,5), xaxs = "i", yaxs = "i")
-# 
-# lines(log10(seq(1,100000,by = 1000)),log10(seq(1,100000,by = 1000)+ (.2* seq(1,100000,by = 1000))), col = 2)
-# lines(log10(seq(1,100000,by = 1000)),log10(seq(1,100000,by = 1000)- (.2* seq(1,100000,by = 1000))), col = 2)
 
 olWidths <- data.frame(queryHitWidth = mcols(allIns.gr)$gapWidth[queryHits(supIns)],
                   subjectHitWidth = mcols(allIns.gr)$gapWidth[subjectHits(supIns)])
 
-# diference between region size is < 10% of smallest regions
+# diference between region size is < 20% of smallest regions
 canIns.gr <- allIns.gr[queryHits(supIns)[ 
   abs(olWidths$queryHitWidth - olWidths$subjectHitWidth) < .2 * apply(X = olWidths,FUN = min,MARGIN = 1)]
   ]
-canIns.gr <- makeGRangesFromDataFrame(unique(as.data.frame(canIns.gr)))
+canIns.gr <- canIns.gr[!(duplicated(mcols(canIns.gr)))]
+
 mcols(canIns.gr)$subjectID <- paste("ins:", 1:length(canIns.gr), sep = "")
 ReduceCanIns.gr <- reduce(resize(canIns.gr, width = 10, fix = "center"))
 
@@ -189,45 +161,6 @@ allCandidates.df <- as.data.frame(allCandidates)
 write.table(x = reducedCandidates.df, file = paste(opt$outDir,"/",opt$queryName,".supportedIndel.merged",sep = "" ), sep = "\t", quote = FALSE,row.names = FALSE, col.names = TRUE)
 
 write.table(x = allCandidates.df, file = paste(opt$outDir,"/",opt$queryName,".supportedIndel.all",sep = "" ), sep = "\t", quote = FALSE,row.names = FALSE, col.names = TRUE)
-
-
-## combine obects
-## Two outputs 
-## Suprted ranges 
-## merged ranges
-
-# 
-# hist(log10((mcols(refIns.gr)$gapWidth)), breaks = 100)
-# hist(log10(df$supGapMean), breaks = 100, add = TRUE, col = 2, density = 0)
-# hist(log10((mcols(refDel.gr)$gapWidth)), breaks = 100)
-# hist(log10((mcols(ReduceCanDel.gr)$supGapMean)), breaks = 100, add = TRUE, col = 2, density = 0)
-# 
-# 
-# 
-# # get all ranges candidates that are sufficiently close to each other
-# 
-# hist(log10(mcols(distanceToNearest(reduce(allDel.gr)))$distance ), breaks = 100)
-# 
-# 
-# refDel.gr[overlapsAny(mcols(refDel.gr)$refRange , mcols(canDel.gr[18])$refRange)]
-# 
-# refDel.gr[overlapsAny(refDel.gr, canDel.gr[18])]
-# 
-# 
-# canDel.gr[overlapsAny(canDel.gr, refDel.gr[overlapsAny(refDel.gr, canDel.gr[18])] )]
-# 
-# 
-# # now we are controlling for various effects.
-# # its probably time to actually look to see if we can confirm any of our mouse and human gaps
-# 
-# 
-# # also need to get the denomonator for the rate
-# # that will be the part of the genome 
-# 
-
-
-
-# should we just merge both tables Ins and Dels, That way the subject hits will make sense
 
 
 
